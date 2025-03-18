b9f3629e3de3016ee0bc27991b9d3aee246c9598
Oghenemaro Ogbaudu <maroprosperofficial@gmail.com>
2025-03-18 07:32:01 +0000

    completed

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const div = document.querySelector('.menu');
-const header = document.querySelector('header');
-div.addEventListener('click', () => {
-    document.querySelector('.nav').classList.toggle('active');
-    
-    if(document.querySelector('.nav').className === 'nav active'){
-        document.querySelector('header').style.marginBottom = '20vh'; 
-    }
-    else{
-        document.querySelector('header').style.marginBottom = '0';
-    }
-});
+    const header = document.querySelector('header');
+    div.addEventListener('click', () => {
+        document.querySelector('.nav').classList.toggle('active');
+        
+        if(document.querySelector('.nav').className === 'nav active'){
+            document.querySelector('header').style.marginBottom = '20vh'; 
+        }
+        else{
+            document.querySelector('header').style.marginBottom = '0';
+        }
+    });
 header.appendChild(div);
 
 
